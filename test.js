b5a2f810d4b536844e98126e61b70f287cde3d9a
Ruy Adorno <ruyadorno@hotmail.com>
2019-10-22 13:37:30 -0400

    BREAKING CHANGE: Supports node@10+
    - Removed Grunt tooling
    - Declares explicit node version support
    - Added unit tests

--- test.js
+++ test.js
@@ -1,5 +1,76 @@
-var log = require('./index.js');
+const assert = require('assert');
+const log = require('./index.js');
 
+function test({ check, expected: { stdio, message } }) {
+    function setup () {
+        log.stdout = log.stderr = {
+            content: '',
+            write(msg) {
+                this.content += `${msg}`;
+            }
+        };
+    }
+
+    function teardown () {
+        log.stdout = process.stdout;
+        log.stderr = process.stderr;
+    }
+
+    setup();
+    check();
+    assert.ok(
+        log[stdio].content.endsWith(`${message}\n`),
+        `${stdio} should print ${message}`
+    );
+    teardown();
+}
+
+// test success msg
+test({
+    check: () => log.success('Success message'),
+    expected: {
+        stdio: 'stdout',
+        message: 'Success message'
+    }
+});
+
+// test error msg
+test({
+    check: () => log.success('Error message'),
+    expected: {
+        stdio: 'stderr',
+        message: 'Error message'
+    }
+});
+
+// test info msg
+test({
+    check: () => log.success('Info message'),
+    expected: {
+        stdio: 'stdout',
+        message: 'Info message'
+    }
+});
+
+// test warn msg
+test({
+    check: () => log.success('Warn message'),
+    expected: {
+        stdio: 'stdout',
+        message: 'Warn message'
+    }
+});
+
+// test simple msg
+test({
+    check: () => log.success('Simple message'),
+    expected: {
+        stdio: 'stdout',
+        message: 'Simple message'
+    }
+});
+
+// Visual feedback, just print these to stdout
 log.success('Success message');
 log.error('Error message');
 log.info('Info message');
